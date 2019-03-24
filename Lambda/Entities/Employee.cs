80198c5012d364ae29387a000606b6e9f64d3e83
Felipe Tadeu Geraldo <felipe.develop@gmail.com>
2019-03-24 16:06:19 -0300

    exercicio expressao lambda + linq curso c#

---
+++ Lambda/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Lambda.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vet = csvEmployee.Split(',');
+
+            Name = vet[0];
+            Email = vet[1];
+            Salary = double.Parse(vet[2], CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Lambda.Entities;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            Console.Write("Enter salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        employees.Add(new Employee(sr.ReadLine()));
+
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Email of people whose salary is more than 2000.00:");
+                var emails = employees.Where(p => p.Salary > salary).OrderBy(p => p.Email).Select(p => p.Email);
+                foreach (string item in emails)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine();
+
+                var sum = employees.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+        }
+    }
+}
